3d64ae6066ab0d0dc9d0b289ec336172e056af51
yashvi9705 <yashvippatel2005@gmail.com>
2024-01-11 21:06:30 -0500

    uopdated the javascript
    added cool to all the elements in the list and added the number of items in the list

--- inft2202/ICE1-Starter/scripts/list.js
+++ inft2202/ICE1-Starter/scripts/list.js
@@ -27,15 +27,25 @@ groceryList.prepend(newListItem2);
 // All <li> elements
 var listItems = groceryList.getElementsByTagName("li");
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
-
+ 
 // Counter variable
+var numberOfItems = listItems.length;
 // Loop through elements
+for(let i =0; i<numberOfItems; i++){
+    listItems[i].classList.add("cool");
+}
 // Change class to cool
 
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var header2 = document.getElementById("header2");
 // h2 element
+
 // h2 text
+let header2Text = header2.textContent;
 // No. of <li> elements
+var numberOfItems = listItems.length;
 // Content
+var content = header2 + " " + numberOfItems;
 // Update h2 using innerHTML (not textContent) because it contains markup
+header2.innerHTML = content;
