962ce3b280bfd82d7206b2a153eef23f1f1de34b
yashvi9705 <yashvippatel2005@gmail.com>
2024-02-16 01:35:36 -0500

    ICE 5
    Initial Commit of the ICE 5

---
+++ inft2202/ICE5_starter/scripts/xhr_script.js
@@ -0,0 +1,29 @@
+console.log('xhr_script.js loaded')
+
+let url_posts = 'https://jsonplaceholder.typicode.com/posts';
+let url_comments = 'https://jsonplaceholder.typicode.com/comments';
+let url_albums = 'https://jsonplaceholder.typicode.com/albums';
+let url_photos = 'https://jsonplaceholder.typicode.com/photos';
+let url_todos = 'https://jsonplaceholder.typicode.com/todos';
+let url_users = 'https://jsonplaceholder.typicode.com/users';
+
+// instantiate the XMLHttpRequest object with the new keyword
+
+
+
+// create a callback function to fire when the onreadystatechange happens
+    // check that the state is done
+    // if (xhr.readyState === 4) {
+            // turn into json
+            // console log to see what we have
+            // update the 1st image
+            // update the 1st figcaption
+            // update the 2nd image
+            // update the 2nd figcaption
+            // send error message
+
+// use the .open() method to configure the object
+
+// add datatype to header
+
+// use the .send() method to send the request
