504fb4e85b8d39e8b634ccea2b2d0345bc5e3226
yashvi9705 <yashvippatel2005@gmail.com>
2024-02-20 23:53:22 -0500

    ICE 6 Initial Commit

---
+++ inft2202/ICE6/scripts/fetch_script.js
@@ -0,0 +1,16 @@
+console.log("fetch_script.js loaded");
+
+// get the button for fetch
+
+// create a url variable
+
+//need a header Accept value set to 'application/json'
+
+// create the callback for the click
+    // use fetch
+        // give the necessary header data
+        // first then() to recieve the promise
+            // send the json from the promise on to the next then()
+            // console log the json
+            // set the output
+
---
+++ inft2202/ICE6/scripts/xhr_script.js
@@ -0,0 +1,19 @@
+console.log("xhr_script.js loaded");
+
+// get the button for xhr
+
+// create a url variable
+
+// create a click callback that handles the API call
+    // instantiate an XMLHttpRequest object
+    // confiture with open()
+    // set the necessary headers for icanhazdadjoke.com
+    // create the callback to handle the onreadystatechange
+        // only execute when done and status is 200
+            // get the responseText
+            // console log
+            // JSON.parse the joke
+            // set the output
+    // send the request
+
+
